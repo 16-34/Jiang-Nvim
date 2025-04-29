8ca19ed48288c6ec7efeb7ae42ffb83b80273d57
Lue Jiang <3103026720@qq.com>
2025-04-29 20:16:55 +0800

    不同操作系统的终端快捷键绑定

--- lua/plugins/vim-floaterm.lua
+++ lua/plugins/vim-floaterm.lua
@@ -1,14 +1,29 @@
 -- 浮动终端
-return {{
+return {
     "voldikss/vim-floaterm",
-    keys = {{
-        "<A-`>",
-        "<CMD>FloatermToggle<CR>",
-        mode = {"n", "t"}
-    }},
+    keys = function()
+        local keymap = {}
+        local os_name = vim.loop.os_uname().sysname
+
+        if os_name == "Darwin" then
+            table.insert(keymap, {
+                "<C-`>",
+                "<CMD>FloatermToggle<CR>",
+                mode = {"n", "t"}
+            })
+        else
+            table.insert(keymap, {
+                "<A-`>",
+                "<CMD>FloatermToggle<CR>",
+                mode = {"n", "t"}
+            })
+        end
+
+        return keymap
+    end,
     config = function()
         vim.g.floaterm_autoclose = 2
         vim.g.floaterm_width = 0.8
         vim.g.floaterm_height = 0.8
     end
-}}
+}
