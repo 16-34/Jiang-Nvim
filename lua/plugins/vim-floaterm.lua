6a75399ea1ecda49046c539611b92ad4d8e1ad1d
Lue Jiang <3103026720@qq.com>
2025-05-21 11:18:39 +0800

    运行代码

--- lua/plugins/vim-floaterm.lua
+++ lua/plugins/vim-floaterm.lua
@@ -25,5 +25,23 @@ return {
         vim.g.floaterm_autoclose = 2
         vim.g.floaterm_width = 0.8
         vim.g.floaterm_height = 0.8
-    end
+
+        vim.api.nvim_create_user_command('RunCode', function()
+            vim.cmd("silent write")
+
+            local ft = vim.bo.filetype
+            if ft == 'python' then
+                vim.cmd('FloatermNew --autoclose=0 python3 "%"')
+            elseif ft == 'c' then
+                vim.cmd('FloatermNew --autoclose=0 gcc "%" -g -o "%<" && "./%<"')
+            elseif ft == 'cpp' then
+                vim.cmd('FloatermNew --autoclose=0 g++ "%" -std=c++11 -g -o "%<" && "./%<"')
+            else
+                print('暂不支持的文件类型: ' .. ft)
+            end
+        end, {
+            desc = "运行当前代码"
+        })
+    end,
+    lazy = false
 }
